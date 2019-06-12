53ed5928a1b39f9a6d1570c8dcb2582b5469d69d
Afsin Ustundag <afsin@amida.com>
2019-06-12 07:56:13 -0400

    remove gulp

--- analysis.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-for filenamme in *; do
-  if [ "$filename" != node_modules ]; then
-    echo module.exports = "'$filenamme'" > readAnalysisFile.js
-    gulp analysis
-  fi
-done
-rm readAnalysisFile.js
